521d98fec83665b3af177c0b924a63bd460f8de7
Tom Brown <nottombrown@gmail.com>
2017-05-13 13:48:01 -0700

    Add instructions

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # cifar10-blobs
+
+BLOBS!
+```
+conda create --name cifar10-blobs python
+source activate cifar10-blobs
+pip install -e .
+```
---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='cifar10_blobs',
+    version='0.0.1',
+    install_requires=[
+        'tensorflow==0.12.1'
+    ]
+)
